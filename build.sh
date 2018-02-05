22210d0345938f1581fbc75c3f41d2978eb2273b
strupo <sheeit@users.noreply.github.com>
2018-02-05 21:44:11 +0100

    Added --help option to build.sh
    Signed-off-by: strupo <sheeit@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -5,6 +5,16 @@
 # Prefix defaults to /usr/local
 # After this scripts completes, run ‘make install’ to install it.
 
+if [ "${#}" -ge 1 ] && ([ "${1}" = '-h' ] || [ "${1}" = '--help' ]); then
+	cat 1>&2 <<-EOF
+		Usage: "${0}" [PREFIX]
+		PREFIX defaults to /usr/local.
+		After this script completes, run ‘make install’ to install
+		You may need to run it as root, depending on the PREFIX.
+EOF
+	exit
+fi
+
 test -d build && rm -rf build && mkdir build
 autoreconf -i && \
 cd build && \
